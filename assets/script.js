aa18c5482d9128c5acb4e280f71788f6a0b39f08
Coding Padawan <gustavohiguera9715@gmail.com>
2022-08-06 12:57:20 -0700

    made the dropdown items interactive

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,12 @@
 var searchBtnEl = $("#search-btn");
 var giveawayBtn = $("#platform-search");
 var cheapsharkContentEl = $("#cheapshark-content")
-var selectPlatformEl = $("#select-platform")
-var searchedGamesEl = $("#searched-games");
 
 var searchedGames = JSON.parse(localStorage.getItem('searchedGames')) || [];
 
-searchBtnEl.on("click", formHandler);
+searchBtnEl.on("click", gameSearchHandler);
 
-function formHandler() {
+function gameSearchHandler() {
   $(cheapsharkContentEl).empty();
   var userInput = $("#search-input").val();
   var userInputFormatted = userInput.replace(/ /g, "+");
@@ -18,16 +16,17 @@ function formHandler() {
     displaySearchedHistory();
 
   }
+
   localStorage.setItem('searchedGames', JSON.stringify(searchedGames));
 
-  $("#giveaway-content").removeClass("hidden");
-
+  $("#giveaway-content").removeClass("hidden");   // when the search btn is clicked, the giveaway content will be visible
 
   cheapsharkApi(userInputFormatted);
 }
 
 function displaySearchedHistory() {
-  // searchedGamesEl.empty();
+  var searchedGamesEl = $("#searched-games");
+
   //Loop through searchedGames array
   searchedGames.forEach(function (game) {
 
@@ -38,22 +37,21 @@ function displaySearchedHistory() {
     btn.text(game)
     searchedGamesEl.append(btn)
 
-    console.log(btn)
 
   })
 }
 
-$(document).on("click", ".searched-game-btn", function(){
-  
+$(document).on("click", ".searched-game-li", function () {
+  $(cheapsharkContentEl).empty();
+
   var game = $(this).data("game");
   cheapsharkApi(game);
 })
 
-displaySearchedHistory();
-
-giveawayBtn.on("click", platformHandler);
+giveawayBtn.on("click", platformHandler); // when the giveaway search button is clicked, the platformHandler function will execute
 
 function platformHandler() {
+  var selectPlatformEl = $("#select-platform")
   var platformValue = selectPlatformEl.val();
 
   gamerPowerApi(platformValue);
@@ -84,43 +82,42 @@ function displayCheapsharkApi(data) {
     cardEl.addClass("card col s12");
     cheapsharkContentEl.append(cardEl);
 
-    var cardTitleEl = document.createElement("div");
-    cardTitleEl.classList.add("card-title");
+    var cardTitleEl = $("<div/>");
+    cardTitleEl.addClass("card-title");
     cardEl.append(cardTitleEl);
 
-    var cardImageEl = document.createElement("div");
-    cardImageEl.classList.add("card-image");
+    var cardImageEl = $("<div/>");
+    cardImageEl.addClass("card-image");
     cardEl.append(cardImageEl);
 
-    var cardContentEl = document.createElement("div");
-    cardContentEl.classList.add("card-content");
+    var cardContentEl = $("<div/>");
+    cardContentEl.addClass("card-content");
     cardEl.append(cardContentEl);
 
-    var cardActionEl = document.createElement("div");
-    cardActionEl.classList.add("card-action");
+    var cardActionEl = $("<div/>");
+    cardActionEl.addClass("card-action");
     cardEl.append(cardActionEl);
 
     var thumbnailUrl = data[i].thumb;
-    var thumbEl = document.createElement("img");
-    thumbEl.setAttribute("src", thumbnailUrl);
-    thumbEl.style.height = "auto";
-    thumbEl.style.width = "auto";
+    var thumbEl = $("<img/>");
+    thumbEl.attr("src", thumbnailUrl);
+    thumbEl.width("auto").height("auto");
     cardImageEl.append(thumbEl);
 
     var gameName = data[i].external;
-    var gameNameEl = document.createElement("h5");
-    gameNameEl.textContent = gameName;
+    var gameNameEl = $("<h5/>");
+    gameNameEl.text(gameName);
     cardTitleEl.append(gameNameEl)
 
     var gamePrice = data[i].cheapest;
-    var gamePriceEl = document.createElement("p");
-    gamePriceEl.textContent = `Cheapest Price: $${gamePrice}`;
+    var gamePriceEl = $("<p/>");
+    gamePriceEl.text(`Cheapest Price: $${gamePrice}`);
     cardContentEl.append(gamePriceEl)
 
-    var dealEl = document.createElement("a");
-    dealEl.textContent = "Click Here to Get Your Deal!";
-    dealEl.setAttribute("href", dealUrl);
-    dealEl.setAttribute("target", "_blank");
+    var dealEl = $("<a/>");
+    dealEl.text("Click Here to Get Your Deal!");
+    dealEl.attr("href", dealUrl);
+    dealEl.attr("target", "_blank");
     cardActionEl.append(dealEl)
   }
 }
@@ -136,7 +133,7 @@ function gamerPowerApi(input) {
   console.log(input)
   fetch(`https://gamerpower.p.rapidapi.com/api/filter?platform=${input}`, options)
     .then(response => response.json())
-    .then(response =>  displayGamerPower(response)
+    .then(response => displayGamerPower(response)
     )
     .catch(err => console.error(err));
 
@@ -151,7 +148,8 @@ $(document).ready(function () {
 });
 
 $('.dropdown-trigger').dropdown({
-  coverTrigger:false,
-  hover:true,
+  coverTrigger: false,
   alignment: 'right'
 });
+
+displaySearchedHistory();
